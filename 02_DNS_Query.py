80044132dd142c74bda923369d904d33981c1067
bernersFT <84954118+bernersFT@users.noreply.github.com>
2022-02-09 23:28:32 +0800

    Update and rename 03_DNS_Query.py to 02_DNS_Query.py

--- 02_DNS_Query.py
+++ 02_DNS_Query.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Date    : 2020-05-05 23:23:51
-# @Author  : Nero (Nero@iv66.net)
+# @Author  : Berners.lk@gmail.com
 # @Link    : N/A
 # @Version : $Id$
 
@@ -19,11 +19,12 @@ def dns_query(domain,type):
 
 
 # （1） A记录， 将主机转换为IP地址
-# （2）MX记录 （邮件交换记录，定义邮件服务器的域名）
-# （3）ns 记录 （标记区域的域名服务器及授权子域） 只限输入一级域名
-# （4）CNAME记录 （指别名记录，实现域名间的映射）
-# dns_query('baidu.com','NS')
-# dns_query('s02.tonglueyun.com','A')
-# dns_query('163.com','MX')
-# dns_query('163.com','CNAME')
+# （2） MX记录 （邮件交换记录，定义邮件服务器的域名）
+# （3） ns 记录 （标记区域的域名服务器及授权子域） 只限输入一级域名
+# （4） CNAME记录 （指别名记录，实现域名间的映射）
+
+dns_query('baidu.com','NS')
+dns_query('s02.tonglueyun.com','A')
+dns_query('163.com','MX')
+dns_query('163.com','CNAME')
 dns_query('www.uwintech.cn','CNAME')
